9555714c6c580bd001150e4802c57c77e77ccd98
augustocfmer <30644606+augustocfmer@users.noreply.github.com>
2025-02-20 11:24:59 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Eu amo você, pitchulinha! Se você me ama, dá uma risadinha! ❤️❤️😎</h1>
+  return (
+    <h1>Eu amo você, pitchulinha! Se você me ama, dá uma risadinha! ❤️❤️😎</h1>
+  );
+}
+
+function teste() {
+  console.log("indentação errada");
 }
 
 export default Home;
